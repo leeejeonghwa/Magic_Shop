f2899b6fd31e260689726b9fe40adb39d8619fe7
jinhui-leee <fkdkfk1578@gmail.com>
2023-11-18 16:13:48 +0900

    CategorySelection activity&xml 추가

---
+++ app/src/main/java/com/example/magic_shop/CategorySelectionActivity.java
@@ -0,0 +1,18 @@
+package com.example.magic_shop;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CategorySelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitt_category_selection);
+
+        // 새 페이지의 동작을 여기에 추가
+    }
+
+
+}
