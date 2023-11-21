51c09d804ab69278fd7ffdfcd8207303b3be19b2
jinhui-leee <fkdkfk1578@gmail.com>
2023-11-22 03:21:15 +0900

    category 버튼 추가

---
+++ app/src/main/java/com/example/magic_shop/CategoryProductListActivity.java
@@ -0,0 +1,17 @@
+package com.example.magic_shop;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CategoryProductListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.category_product_list);
+
+
+    }
+
+}
--- app/src/main/java/com/example/magic_shop/CategorySelectionActivity.java
+++ app/src/main/java/com/example/magic_shop/CategorySelectionActivity.java
@@ -1,6 +1,8 @@
 package com.example.magic_shop;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,9 +11,18 @@ public class CategorySelectionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activitt_category_selection);
+        setContentView(R.layout.category_selection);
 
         // 새 페이지의 동작을 여기에 추가
+
+        findViewById(R.id.btn1_id).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 버튼을 클릭했을 때 SecondActivity로 이동하는 Intent 생성
+                Intent intent = new Intent(CategorySelectionActivity.this, CategoryProductListActivity.class);
+                startActivity(intent); // Intent를 사용하여 SecondActivity 시작
+            }
+        });
     }
 
 
