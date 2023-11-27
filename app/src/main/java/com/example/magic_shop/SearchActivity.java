805e83bafc3aaa91800bda5727e7666efa2d74a9
dlekgus <97431420+dlekgus@users.noreply.github.com>
2023-11-28 00:18:55 +0900

    검색창

---
+++ app/src/main/java/com/example/magic_shop/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.example.magic_shop;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState){
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
+    }
+}
