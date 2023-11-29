10e68108d8b153ef5a306b632100c4638ce54d24
jinhui-leee <fkdkfk1578@gmail.com>
2023-11-29 21:54:35 +0900

    주문 결제 페이지 추가

---
+++ app/src/main/java/com/example/magic_shop/OrderFormActivity.java
@@ -0,0 +1,17 @@
+package com.example.magic_shop;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrderFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_form);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/magic_shop/ShoppingBasketActivity.java
@@ -0,0 +1,16 @@
+package com.example.magic_shop;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShoppingBasketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shopping_basket);
+    }
+
+
+}
