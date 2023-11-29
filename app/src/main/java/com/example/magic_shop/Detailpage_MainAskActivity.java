a4822a02fc31f489efe082e643cc82a9727b2dd9
Java-Baekjoon <dlwjdghk133@naver.com>
2023-11-30 00:19:55 +0900

    detailpage 작업 완료

--- app/src/main/java/com/example/magic_shop/Detailpage_MainAskActivity.java
+++ app/src/main/java/com/example/magic_shop/Detailpage_MainAskActivity.java
@@ -8,30 +8,35 @@ import android.view.View;
 import android.widget.Button;
 
 
-public class Detailpage_MainActivity extends AppCompatActivity {
+public class Detailpage_MainAskActivity extends AppCompatActivity {
 
     private Button btnBuy;
     private Button btnReview;
     private Button btnSize;
-    private Button btnAsk;
+    private Button btnProduct;
+    private Button btnProductAsk;
+    private Button btnOrderAsk;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.detailpage_activity_main);
+        setContentView(R.layout.detailpage_activity_main_ask);
 
         btnBuy = findViewById(R.id.btn_buy);
         btnReview = findViewById(R.id.reviewBtn);
+        btnProduct = findViewById(R.id.productBtn);
         btnSize = findViewById(R.id.sizeBtn);
-        btnAsk = findViewById(R.id.askBtn);
+        btnProductAsk = findViewById(R.id.askProductBtn);
+        btnOrderAsk = findViewById(R.id.askOrdertBtn);
 
         btnBuy.setVisibility(View.VISIBLE);
         btnReview.setVisibility(View.VISIBLE);
+        btnProduct.setVisibility(View.VISIBLE);
         btnSize.setVisibility(View.VISIBLE);
-        btnAsk.setVisibility(View.VISIBLE);
-
+        btnProductAsk.setVisibility(View.VISIBLE);
+        btnOrderAsk.setVisibility(View.VISIBLE);
 
         // btnBuy의 클릭 이벤트 처리
         btnBuy.setOnClickListener(new View.OnClickListener() {
@@ -50,6 +55,14 @@ public class Detailpage_MainActivity extends AppCompatActivity {
             }
         });
 
+        btnProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Detailpage_MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
         btnSize.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,13 +71,22 @@ public class Detailpage_MainActivity extends AppCompatActivity {
             }
         });
 
-        btnAsk.setOnClickListener(new View.OnClickListener() {
+        btnProductAsk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),Detailpage_MainAskActivity.class);
+                Intent intent = new Intent(getApplicationContext(),Detailpage_MainSizeActivity.class);
                 startActivity(intent);
             }
         });
+
+        btnOrderAsk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Detailpage_MainSizeActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 
