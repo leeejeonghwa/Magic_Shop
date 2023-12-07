b2cb08b4e3b3b96150edd9422b3ffae75ddde1f3
kimsojeong <rtw0202@naver.com>
2023-12-07 21:14:52 +0900

    후기 입력하면 db 저장, productID는 후기 작성 전 상품 목록 페이지에서 intent로 넘겨서 가져올 예정

--- app/src/main/java/com/example/magic_shop/Mypage_ReviewWriteActivity.java
+++ app/src/main/java/com/example/magic_shop/Mypage_ReviewWriteActivity.java
@@ -1,29 +1,52 @@
 package com.example.magic_shop;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Mypage_ReviewWriteActivity extends AppCompatActivity {
 
+    private EditText editTextContent, editTextScore;
+    private Response.ErrorListener errorListener;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage_activity_review_write);
         getWindow().setWindowAnimations(0);
+
+        SessionManager sessionManager = new SessionManager(getApplicationContext());
+        String userID = sessionManager.getUserId();
+        String writer = sessionManager.getUserNickname();
+
+        editTextContent = findViewById(R.id.editTextContent);
+        editTextScore = findViewById(R.id.editTextScore);
 
         Button btn_back = (Button) findViewById(R.id.btn_back);
         btn_back.setOnClickListener(new View.OnClickListener() {
@@ -40,16 +63,108 @@ public class Mypage_ReviewWriteActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Mypage_ReviewedListActivity.class);
-                startActivity(intent);
+                String comment = editTextContent.getText().toString();
+                String score = editTextScore.getText().toString();
+
+                if (!comment.isEmpty() &&
+                        !score.isEmpty()) {
+
+                    plusReview(
+//                            productID,
+                            userID,
+                            writer,
+                            comment,
+                            score
+                    );
+
+                    Intent intent = new Intent(getApplicationContext(), Mypage_ReviewedListActivity.class);
+                    startActivity(intent);
+
+                } else {
+                    showAlert("모든 필드를 채워주세요.");
+                }
             }
         });
+    }
 
-        Intent intent = getIntent();
+    private void handleNonJsonResponse(String response) {
+        try {
+            JSONObject jsonResponse = new JSONObject(response);
+//            String productID = jsonResponse.getString("productID");
+            String userID = jsonResponse.getString("userID");
+            String writer = jsonResponse.getString("writer");
+            String content = jsonResponse.getString("content");
+            String score = jsonResponse.getString("score");
 
-        TextView productName = findViewById(R.id.productName);
+            // 텍스트 뷰에 값을 설정합니다.
+//            editTextProductID.setText(productID);
+//            editTextRecipient.setText(recipient);
+//            editTextPhoneNumber.setText(phoneNumber);
+//            editTextAddress.setText(address);
+//            editTextDeliveryAddressRequest.setText(deliveryRequest);
 
-        productName.setText(intent.getStringExtra("productName"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "서버 응답 형식 오류로 리뷰 등록에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @SuppressLint("LongLogTag")
+    private void plusReview(String userID, String writer, String content, String score) {
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @SuppressLint("LongLogTag")
+            @Override
+            public void onResponse(String response) {
+                try {
+                    Log.d("Mypage_ReviewWriteActivity", " plusDeliveryAddress() 서버 응답: " + response);
+
+                    if (response.startsWith("<br")) {
+                        handleNonJsonResponse(response);
+                        return;
+                    }
+
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("success");
+                    if (success) {
+                        String successMessage = "리뷰 등록에 성공하였습니다.";
+                        Toast.makeText(getApplicationContext(), successMessage, Toast.LENGTH_SHORT).show();
+                        finish();
+                        Intent intent = new Intent(getApplicationContext(), Mypage_ReviewedListActivity.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "리뷰 등록에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.e("Mypage_ReviewWriteActivity", "JSON 파싱 오류: " + e.getMessage());
+                    Toast.makeText(getApplicationContext(), "서버 응답 형식 오류로 리뷰 등록에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.e("Mypage_ReviewWriteActivity", "예외 발생: " + e.getMessage());
+                    Toast.makeText(getApplicationContext(), "알 수 없는 오류로 리뷰 등록에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        try {
+            ReviewPlusRequest ReviewPlusRequest = new ReviewPlusRequest(userID,
+                    writer, content, score, responseListener, errorListener);
+            RequestQueue queue = Volley.newRequestQueue(Mypage_ReviewWriteActivity.this);
+            queue.add(ReviewPlusRequest);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            Log.e("Mypage_ReviewWriteActivity", "plusReviewWrite 함수 내부에서 예외 발생: " + e.getMessage());
+        }
+    }
+
+    private void showAlert(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Mypage_ReviewWriteActivity.this);
+        builder.setMessage(message)
+                .setNegativeButton("다시 시도", null)
+                .create()
+                .show();
     }
 }
 
