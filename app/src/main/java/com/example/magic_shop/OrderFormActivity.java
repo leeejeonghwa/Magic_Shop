e292b513881a7259995b1355681ff39b0ae05b47
AISELAB <jeju3146@naver.com>
2023-12-09 03:39:29 +0900

    Create an order form DB connection

--- app/src/main/java/com/example/magic_shop/OrderFormActivity.java
+++ app/src/main/java/com/example/magic_shop/OrderFormActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -36,6 +37,9 @@ public class OrderFormActivity extends AppCompatActivity {
     private String selectedPaymentMethod; // 사용자가 선택한 결제 수단을 저장하는 변수
     private Response.ErrorListener errorListener;
     private TextView totalPriceTextView;
+    private int totalAmount; // totalAmount 변수를 선언
+
+    private PurchaseAdapter adapter; // 어댑터 변수를 여기에 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,37 +57,17 @@ public class OrderFormActivity extends AppCompatActivity {
             }
         });
 
+
+
         // 2. 체크박스 초기화
         checkBoxCreditCard = findViewById(R.id.check_card);
         checkBoxAccount = findViewById(R.id.check_account);
         checkBoxBankDeposit = findViewById(R.id.check_deposit);
 
         // 3. 체크박스 상태 변경 시 이벤트 처리
-        checkBoxCreditCard.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                selectedPaymentMethod = "C";
-                // 다른 체크박스 상태 초기화
-                checkBoxAccount.setChecked(false);
-                checkBoxBankDeposit.setChecked(false);
-            }
-        });
-
-        checkBoxAccount.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                selectedPaymentMethod = "A";
-                checkBoxCreditCard.setChecked(false);
-                checkBoxBankDeposit.setChecked(false);
-            }
-        });
-
-        checkBoxBankDeposit.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                selectedPaymentMethod = "B";
-                checkBoxCreditCard.setChecked(false);
-                checkBoxAccount.setChecked(false);
-            }
-        });
-
+        checkBoxCreditCard.setOnCheckedChangeListener((buttonView, isChecked) -> handleCheckboxChecked(isChecked, "C"));
+        checkBoxAccount.setOnCheckedChangeListener((buttonView, isChecked) -> handleCheckboxChecked(isChecked, "A"));
+        checkBoxBankDeposit.setOnCheckedChangeListener((buttonView, isChecked) -> handleCheckboxChecked(isChecked, "B"));
 
         TextViewAddressName = findViewById(R.id.shipping_address_name);
         TextViewRecipient = findViewById(R.id.shipping_recipient);
@@ -94,16 +78,14 @@ public class OrderFormActivity extends AppCompatActivity {
 
         getDefaultDeliveryAddress(userID);
 
-
         RecyclerView recyclerView = findViewById(R.id.purchase_recycler);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        PurchaseAdapter adapter = new PurchaseAdapter(new ArrayList<>(), this, totalPriceTextView);
+        adapter = new PurchaseAdapter(new ArrayList<>(), this, totalPriceTextView);
         recyclerView.setAdapter(adapter);
 
         Intent intent = getIntent();
-
 
         ArrayList<String> selectedBrands = intent.getStringArrayListExtra("SELECTED_BRANDS");
         ArrayList<String> selectedProductNames = intent.getStringArrayListExtra("SELECTED_PRODUCT_NAMES");
@@ -111,11 +93,14 @@ public class OrderFormActivity extends AppCompatActivity {
         ArrayList<String> selectedPrice = intent.getStringArrayListExtra("SELECTED_PRICE");
         ArrayList<Integer> selectedProductID = intent.getIntegerArrayListExtra("SELECTED_PRODUCT_ID");
 
+
+
         if (intent != null) {
             int totalItemCount = intent.getIntExtra("TOTAL_ITEM_COUNT", 0);
-            int totalPrice = intent.getIntExtra("TOTAL_PRICE", 0);
+            final int totalPrice = intent.getIntExtra("TOTAL_PRICE", 0);
             totalPriceTextView.setText("결제할 상품 총 " + totalItemCount + " 개" +
                     "\n상품 금액 " + totalPrice + "원");
+
 
             // PurchaseItem 생성 및 어댑터에 추가
             for (int i = 0; i < selectedBrands.size(); i++) {
@@ -127,10 +112,46 @@ public class OrderFormActivity extends AppCompatActivity {
                         selectedProductID.get(i)
                 );
                 adapter.addPurchaseItem(purchaseItem);
-
             }
         }
+        findViewById(R.id.btn_purchase).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int totalPrice = intent.getIntExtra("TOTAL_PRICE", 0);
+                String selectedPaymentMethod = getSelectedPaymentMethod();
+
+                // 결제 수단이 선택되었을 때만 주문 처리를 수행
+                if (selectedPaymentMethod != null && !selectedPaymentMethod.isEmpty()) {
+                    // 주문하기 버튼이 클릭되었을 때 실행되는 코드
+                    sendOrder(userID, totalPrice, selectedPaymentMethod);
+
+                    // 주문 처리 후 페이지 이동 (예: 다음 화면으로 이동)
+                    Intent intent = new Intent(OrderFormActivity.this, PaymentCompleteActivity.class);
+                    intent.putExtra("ORDER_SUCCESSFUL", true); // You can pass additional data if needed
+                    startActivity(intent);
+                    finish(); // Optional: Finish the current activity if you don't want to go back to it
+                } else {
+                    // 선택된 결제 수단이 없을 때 사용자에게 메시지 표시 또는 처리
+                    Toast.makeText(OrderFormActivity.this, "결제 수단을 선택하세요.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+    private void handleCheckboxChecked(boolean isChecked, String paymentMethod) {
+        if (isChecked) {
+            selectedPaymentMethod = paymentMethod;
+            // 다른 체크박스 상태 초기화
+            checkBoxAccount.setChecked(false);
+            checkBoxBankDeposit.setChecked(false);
+        }
+    }
+
+    // 선택된 결제 수단을 반환하는 메소드
+    private String getSelectedPaymentMethod() {
+        return selectedPaymentMethod;
+    }
+
 
     private void getDefaultDeliveryAddress(String userID) {
         Response.Listener<String> responseListener = new Response.Listener<String>() {
@@ -199,6 +220,65 @@ public class OrderFormActivity extends AppCompatActivity {
         queue.add(defaultDeliveryAddressGetRequest);
     }
 
+
+    // OrderFormActivity 내에 새로 추가한 메소드
+    private void sendOrder(String userID, int totalAmount, String selectedPaymentMethod) {
+        // 사용자 ID를 세션에서 가져오거나 로그인 정보 등을 이용하여 설정
+
+        // 결제 수단 선택 여부 확인
+        if (selectedPaymentMethod == null) {
+            // 사용자에게 결제 수단을 선택하도록 메시지 표시 또는 처리
+            // 예: Toast 메시지 표시
+            Toast.makeText(this, "결제 수단을 선택하세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // 주문 상품 목록
+        List<Integer> selectedProductIDs = new ArrayList<>();
+        for (PurchaseItem purchaseItem : adapter.getPurchaseList()) {
+            selectedProductIDs.add(purchaseItem.productID);
+        }
+
+        // 서버에 주문 정보 전송
+        SaveOrderRequest saveOrderRequest = new SaveOrderRequest(
+                userID,
+                selectedProductIDs,
+                totalAmount,  // 총 결제 금액 (이 값을 서버에서 사용)
+                selectedPaymentMethod,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // 서버 응답 처리
+                        try {
+                            // 서버 응답 로그
+                            Log.d("OrderFormActivity", "주문 서버 응답: " + response);
+
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+
+                            if (success) {
+                                // 주문 성공
+                                Toast.makeText(OrderFormActivity.this, "주문이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                                // 주문 완료 후 필요한 처리 수행
+                                // 예: 주문 완료 화면으로 이동 또는 다른 작업 수행
+                            } else {
+                                // 주문 실패
+                                Toast.makeText(OrderFormActivity.this, "주문에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                                // 실패 시 처리 수행
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                errorListener
+        );
+
+        // Volley RequestQueue에 요청 추가
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(saveOrderRequest);
+    }
+
     public class PurchaseItem {
         String brandName;
         String productName;
@@ -206,7 +286,7 @@ public class OrderFormActivity extends AppCompatActivity {
         String price;
         Integer productID;
 
-        public PurchaseItem(String brandName, String productName, String option, String price,int productID) {
+        public PurchaseItem(String brandName, String productName, String option, String price, int productID) {
             this.productName = productName;
             this.brandName = brandName;
             this.option = option;
@@ -230,6 +310,11 @@ public class OrderFormActivity extends AppCompatActivity {
         public void addPurchaseItem(PurchaseItem purchaseItem) {
             purchaseList.add(purchaseItem);
             notifyDataSetChanged(); // 데이터 변경 알림
+        }
+
+        // PurchaseList 반환하는 메서드 추가
+        public List<PurchaseItem> getPurchaseList() {
+            return purchaseList;
         }
 
         @NonNull
