6dabb120127d35d9c53178a82efa5b02090519b0
AISELAB <jeju3146@naver.com>
2023-12-10 00:48:59 +0900

    판매자 주문 내역 확인

--- app/src/main/java/com/example/magic_shop/Seller_OrderDeliveryListActivity.java
+++ app/src/main/java/com/example/magic_shop/Seller_OrderDeliveryListActivity.java
@@ -2,11 +2,16 @@ package com.example.magic_shop;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,38 +19,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Seller_OrderDeliveryListActivity extends AppCompatActivity {
 
-    public List<OrderItem> getOrderList() {
-        List<OrderItem> orderList = new ArrayList<>();
+    private List<SellerOrderItem> sellerOrderList;
+    private SellerOrderAdapter adapter;
 
-        // 예시 데이터를 추가합니다. 실제 데이터는 여기서 가져와야 합니다.
-        orderList.add(new OrderItem("2023-11-20", "상품 A", "000000000000"));
-        orderList.add(new OrderItem("2023-11-21", "상품 B", "111111111111"));
-        orderList.add(new OrderItem("2023-11-22", "상품 C", "222222222222"));
-        orderList.add(new OrderItem("2023-11-23", "상품 D", "222222222222"));
-        orderList.add(new OrderItem("2023-11-24", "상품 E", "222222233333"));
-        // ... 추가적인 데이터
-
-        return orderList;
-    }
-
-    public Context context;
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.seller_activity_order_delivery_list);
         getWindow().setWindowAnimations(0);
 
-        Button btn_back = (Button) findViewById(R.id.btn_back);
-        btn_back.setOnClickListener(new View.OnClickListener() {
+        SessionManager sessionManager = new SessionManager(getApplicationContext());
 
+        Button btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onBackPressed();
+                Intent intent = new Intent(getApplicationContext(), Mypage_MainActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -53,88 +57,175 @@ public class Seller_OrderDeliveryListActivity extends AppCompatActivity {
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        List<OrderItem> orderList = getOrderList();
-        OrderAdapter adapter = new OrderAdapter(orderList, this);
+        sellerOrderList = new ArrayList<>();
+        adapter = new SellerOrderAdapter(sellerOrderList, this);
         recyclerView.setAdapter(adapter);
+
+        // 사용자 아이디 (실제 사용자 아이디로 변경)
+        String userID = sessionManager.getUserId();
+
+        // 주문 데이터 가져오기
+        getSellerOrderData(userID, this);
     }
 
-    public class OrderItem {
-        String date;
-        String productName;
-        String trackingNumber;
+    public void getSellerOrderData(String userID, Context context) {
+        // Volley 요청 큐 생성
+        RequestQueue queue = Volley.newRequestQueue(context);
 
-        public OrderItem(String date, String productName, String trackingNumber) {
-            this.date = date;
+        // 주문 데이터 요청
+        GetSellerOrderDataRequest request = new GetSellerOrderDataRequest(userID,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // 응답을 처리하는 코드
+                        Log.d("OrderDetails", "Volley Response: " + response);
+
+                        try {
+                            // JSON 응답 파싱
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+
+                            if (success) {
+                                // 주문 데이터가 있는 경우
+                                JSONArray ordersArray = jsonObject.getJSONArray("sellerorders");
+                                List<SellerOrderItem> sellerOrderList = getSellerOrderList(ordersArray);
+
+                                // 어댑터 갱신
+                                adapter.setSellerOrderList(sellerOrderList);
+                                adapter.notifyDataSetChanged();
+                            } else {
+                                // 주문 데이터가 없는 경우
+                                String message = jsonObject.getString("message");
+                                Log.e("SellerOrderDetails", "Error: " + message);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.e("SellerOrderDetails", "JSON Parsing Error: " + e.getMessage());
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("OrderDetails", "Volley Error: " + error.getMessage());
+                    }
+                });
+
+        // 요청을 Volley 큐에 추가
+        queue.add(request);
+    }
+
+    public List<SellerOrderItem> getSellerOrderList(JSONArray jsonArray) {
+        List<SellerOrderItem> sellerOrderList = new ArrayList<>();
+
+        try {
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject sellerOrderObject = jsonArray.getJSONObject(i);
+
+                // 주문 세부 정보 추출
+                int orderID = sellerOrderObject.getInt("orderID");
+                String userID= sellerOrderObject.getString("customerID");
+                String userName = sellerOrderObject.getString("customerName");
+                String paymentDate = sellerOrderObject.getString("paymentDate");
+                String product_name = sellerOrderObject.getString("product_name");
+                int productPrice = sellerOrderObject.getInt("product_price");
+
+
+
+                SellerOrderItem sellerOrderItem = new SellerOrderItem(orderID,userID,userName,paymentDate,product_name,productPrice);
+                sellerOrderList.add(sellerOrderItem);
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return sellerOrderList;
+    }
+
+    public class SellerOrderItem {
+        String userId;
+        int productPrice;
+        int orderId;
+        String paymentDate;
+
+        String userName;
+
+
+        String productName;
+
+
+
+
+
+
+        public SellerOrderItem(int orderId, String userId, String userName,String paymentDate, String productName ,int productPrice) {
+            this.orderId = orderId;
+            this.userId = userId;
+            this.userName = userName;
+            this.paymentDate = paymentDate;
             this.productName = productName;
-            this.trackingNumber = trackingNumber;
+            this.productPrice = productPrice;
+
         }
     }
 
-    public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.OrderViewHolder> {
-        private List<OrderItem> orderList;
+    public class SellerOrderAdapter extends RecyclerView.Adapter<SellerOrderAdapter.SellerOrderViewHolder> {
+        private List<SellerOrderItem> sellerOrderList;
         private Context context;
 
-        OrderAdapter(List<OrderItem> orderList, Context context) {
-            this.orderList = orderList;
+        SellerOrderAdapter(List<SellerOrderItem> sellerOrderList, Context context) {
+            this.sellerOrderList = sellerOrderList;
             this.context = context;
         }
 
         @NonNull
         @Override
-        public OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        public SellerOrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             Context context = parent.getContext();
-            View view = LayoutInflater.from(context).inflate(R.layout.seller_item_order, parent, false);
-            return new OrderViewHolder(view, context);
+            View view = LayoutInflater.from(context).inflate(R.layout.seller_order_item, parent, false);
+            return new SellerOrderViewHolder(view, context);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull OrderViewHolder holder, int position) {
-            OrderItem orderItem = orderList.get(position);
-            holder.bind(orderItem);
+        public void onBindViewHolder(@NonNull SellerOrderViewHolder holder, int position) {
+            SellerOrderItem  sellerOrderItem = sellerOrderList.get(position);
+            holder.bind(sellerOrderItem);
         }
 
         @Override
-        public int getItemCount() { return orderList.size(); }
+        public int getItemCount() {
+            return sellerOrderList.size();
+        }
 
-        public class OrderViewHolder extends RecyclerView.ViewHolder {
+        public void setSellerOrderList(List<SellerOrderItem> orderList) {
+            this.sellerOrderList = orderList;
+        }
+
+        public class SellerOrderViewHolder extends RecyclerView.ViewHolder {
             private final TextView dateTextView;
-            private final TextView productNameTextView;
-            private final TextView trackingNumberTextView;
-            public final Button deliveryCheckButton;
-            public final Button reviewWriteButton;
+            private final TextView productNameTextView, productPriceTextView, userNameTextView,userIdTextView;
+
             private final Context context;
 
-            public OrderViewHolder(View itemView, Context context) {
+            public SellerOrderViewHolder(View itemView, Context context) {
                 super(itemView);
                 this.context = context;
                 dateTextView = itemView.findViewById(R.id.order_date);
-                productNameTextView = itemView.findViewById(R.id.order_productName);
-                trackingNumberTextView = itemView.findViewById(R.id.order_trackingNumber);
-                deliveryCheckButton = itemView.findViewById(R.id.btn_deliveryCheck);
-                reviewWriteButton = itemView.findViewById(R.id.btn_review_write);
+                productNameTextView = itemView.findViewById(R.id.tv_product_name);
+                productPriceTextView = itemView.findViewById(R.id.tv_product_price);
+                userNameTextView = itemView.findViewById(R.id.tv_user_name);
+                userIdTextView= itemView.findViewById(R.id.tv_user_id);
 
-                deliveryCheckButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // 클릭 이벤트 처리
-                        int position = getAdapterPosition();
-                        if (position != RecyclerView.NO_POSITION) {
-                            // 다음 화면으로 이동하는 코드
-                            OrderItem orderItem = orderList.get(position);
-                            Intent intent = new Intent(context, Mypage_DeliveryCheckActivity.class);
-                            intent.putExtra("date", orderItem.date);
-                            intent.putExtra("productName", orderItem.productName);
-                            intent.putExtra("trackingNumber", orderItem.trackingNumber);
-                            context.startActivity(intent);
-                        }
-                    }
-                });
             }
 
-            void bind(OrderItem orderItem) {
-                dateTextView.setText(orderItem.date);
-                productNameTextView.setText(orderItem.productName);
-                trackingNumberTextView.setText(orderItem.trackingNumber);
+            void bind(SellerOrderItem sellerOrderItem) {
+                dateTextView.setText(sellerOrderItem.paymentDate);
+                userNameTextView.setText(sellerOrderItem.userName);
+                userIdTextView.setText(sellerOrderItem.userId);
+                productNameTextView.setText(sellerOrderItem.productName);
+                productPriceTextView.setText((String.valueOf(sellerOrderItem.productPrice))+"원");
+
             }
         }
     }
